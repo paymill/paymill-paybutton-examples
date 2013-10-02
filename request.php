756e96c3f80c46f1825ad136428aa938829fe339
Thilo Hoffstadt <thilo.hoffstadt@payintelligent.de>
2013-10-02 15:35:08 +0200

    PM-647 Created 1 File Solution for the button implementation.

--- request.php
+++ request.php
@@ -2,7 +2,7 @@
 //Please modify the following variables
 $amount = '250'; // E.g. "250" for 2.50 EUR!
 $currency = 'EUR'; // ISO 4217
-$description = 'Testdescription';
+$description = $_SERVER["HTTP_REFERER"];
 $privateApiKey = 'TESTPRIVATEKEY';
 
 //If you want to use subscriptions please also edit the following variables
@@ -13,60 +13,118 @@ $offerName = 'Testoffer'; //Your name for this offer
 if (isset($_POST['paymillToken'])) {
     $token = $_POST['paymillToken'];
 
-    require "lib/Services/Paymill/Clients.php";
-    require "lib/Services/Paymill/Payments.php";
+    $client = request(
+        'clients/',
+        array(),
+        $privateApiKey
+    );
 
-    $clientsObject = new Services_Paymill_Clients($privateApiKey, "https://api.paymill.com/v2/");
-    $client = $clientsObject->create();
+    $payment = request(
+        'payments/',
+        array(
+             'token' => $token,
+             'client' => $client['id']
+        ),
+        $privateApiKey
+    );
 
-    $paymentsObject = new Services_Paymill_Payments($privateApiKey, "https://api.paymill.com/v2/");
-    $payment = $paymentsObject->create(array(
-        'token' => $token,
-        'client' => $client['id']
-    ));
+    $transaction = request(
+        'transactions/',
+        array(
+             'amount' => $amount,
+             'currency' => $currency,
+             'client' => $client['id'],
+             'payment' => $payment['id'],
+             'description' => $description
+        ),
+        $privateApiKey
+    );
 
-    if($subscription) {
-        require "lib/Services/Paymill/Offers.php";
-        require "lib/Services/Paymill/Subscriptions.php";
-
-        $offersObject = new Services_Paymill_Offers($privateApiKey, "https://api.paymill.com/v2/");
-        $offer = $offersObject->create(array(
-            'amount' => $amount,
-            'currency' => $currency,
-            'interval' => $interval,
-            'name' => $offerName
-        ));
-
-        $subscriptionsObject = new Services_Paymill_Subscriptions($privateApiKey, "https://api.paymill.com/v2/");
-        $subscription = $subscriptionsObject->create(array(
-            'client' => $client['id'],
-            'offer' => $offer['id'],
-            'payment' => $payment['id']
-        ));
-
-        if (isset($subscription['offer']['subscription_count']['active']) && ($subscription['offer']['subscription_count']['active'])) {
-            echo '<strong>Subscription successful!</strong>';
-        } else {
-            echo '<strong>Subscription not successful!</strong>';
-
-        }
-
+    if (isset($transaction['status']) && ( $transaction['status'] == 'closed')) {
+        echo '<strong>Transaction successful!</strong>';
     } else {
-        require 'lib/Services/Paymill/Transactions.php';
-        $transactionsObject = new Services_Paymill_Transactions($privateApiKey, "https://api.paymill.com/v2/");
-
-        $transaction = $transactionsObject->create(array(
-            'amount' => $amount,
-            'currency' => $currency,
-            'client' => $client['id'],
-            'payment' => $payment['id'],
-            'description' => $description
-        ));
-
-        if (isset($transaction['status']) && ( $transaction['status'] == 'closed')) {
-            echo '<strong>Transaction successful!</strong>';
-        } else {
-            echo '<strong>Transaction not successful!</strong>';
-        }
+        echo '<strong>Transaction not successful!</strong>';
+        var_dump($client);
+        var_dump($payment);
+        var_dump($transaction);
     }
 }
+
+    /**
+     * Perform HTTP request to REST endpoint
+     *
+     * @param string $action
+     * @param array $params
+     * @param string $privateApiKey
+     * @return array
+     */
+    function requestApi($action = '', $params = array(), $privateApiKey)
+    {
+        $curlOpts = array(
+            CURLOPT_URL => "https://api.paymill.com/v2/" . $action,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_USERAGENT => 'Paymill-php/0.0.2',
+            CURLOPT_SSL_VERIFYPEER => true,
+            CURLOPT_CAINFO => realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'paymill.crt',
+        );
+
+        $curlOpts[CURLOPT_POSTFIELDS] = http_build_query($params, null, '&');
+        $curlOpts[CURLOPT_USERPWD] = $privateApiKey . ':';
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $curlOpts);
+        $responseBody = curl_exec($curl);
+        $responseInfo = curl_getinfo($curl);
+        if ($responseBody === false) {
+            $responseBody = array('error' => curl_error($curl));
+        }
+        curl_close($curl);
+
+        if ('application/json' === $responseInfo['content_type']) {
+            $responseBody = json_decode($responseBody, true);
+        }
+
+        return array(
+            'header' => array(
+                'status' => $responseInfo['http_code'],
+                'reason' => null,
+            ),
+            'body' => $responseBody
+        );
+    }
+
+    /**
+     * Perform API and handle exceptions
+     *
+     * @param $action
+     * @param array $params
+     * @param string $privateApiKey
+     * @return mixed
+     */
+    function request($action, $params = array(), $privateApiKey)
+    {
+        if (!is_array($params))
+            $params = array();
+
+        $responseArray = requestApi($action, $params, $privateApiKey);
+        $httpStatusCode = $responseArray['header']['status'];
+        if ($httpStatusCode != 200) {
+            $errorMessage = 'Client returned HTTP status code ' . $httpStatusCode;
+            if (isset($responseArray['body']['error'])) {
+                $errorMessage = $responseArray['body']['error'];
+            }
+            $responseCode = '';
+            if (isset($responseArray['body']['response_code'])) {
+                $responseCode = $responseArray['body']['response_code'];
+            }
+
+            return array("data" => array(
+                "error" => $errorMessage,
+                "response_code" => $responseCode,
+                "http_status_code" => $httpStatusCode
+            ));
+        }
+
+        return $responseArray['body']['data'];
+    }
